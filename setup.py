e4e7fd4ef8784c901348a3ad757a6e2a3c7bad7c
Welliton Souza <well309@gmail.com>
2019-10-09 15:17:59 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='HPexome',
-    version='1.1.1',
+    version='1.1.2',
     author="Welliton Souza",
     author_email="well309@gmail.com",
     description="An automated tool for processing whole-exome sequencing data",
